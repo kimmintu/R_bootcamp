8a1b8440c0d81f38804aae99068cd5f87a9ce98e
Andy Gubser <andy.gubser@sealedair.com>
2020-02-13 14:54:14 +0100

    new files

---
+++ bike-share-analysis/Rscript/_old/data_preprocessing.R
@@ -0,0 +1,113 @@
+# Install and load packages
+
+# library(checkpoint)
+# checkpoint(snapshotDate = "2020-01-01")
+
+###### LOAD PACKAGES ######
+# create function
+# packages <- 
+
+if(!require("osmdata")) install.packages("osmdata")
+if(!require("tidyverse")) install.packages("tidyverse")
+if(!require("sf")) install.packages("sf")
+if(!require("ggmap")) install.packages("ggmap")
+# citation("ggmap")
+
+
+#load packages
+library("tidyverse")
+library("osmdata")
+library("sf")
+library("ggmap")
+
+library("tidyverse")
+library("lubridate")
+#library("leaflet")
+library("Hmisc")
+#library("geosphere
+
+
+###### LOAD DATA ######
+# setwd("C:/Users/Andy Gubser/OneDrive - Hochschule Luzern/01 Studium/03 MSc Data Science/Master HS19/Wahlpflichtmodule/W.MSCIDS_RB01.H1901/Assignment/R_bootcamp/bike-share-analysis/Rscript")
+d.bike.raw = read_csv("../data/NYC-CitiBike-2016.csv")
+colnames(d.bike.raw)
+# describe(d.bike.raw)
+
+
+###### CONVERT DATA ######
+
+d.bike <- d.bike.raw %>% select_all(snakecase::to_snake_case)
+
+d.bike <- d.bike %>%
+  mutate(starttime = mdy_hms(starttime),
+         stoptime = mdy_hms(stoptime),
+         gender = factor(gender, 
+                         levels = c(0,1,2), 
+                         labels = c("X", "M", "F")
+                         ) ,
+         start_station_id = factor(start_station_id),
+         start_station_name = factor(start_station_name),
+         end_station_id = factor(end_station_id),
+         end_station_name = factor(end_station_name),
+         bikeid = factor(bikeid),
+         usertype = factor(usertype),
+         birth_year = factor(birth_year),
+         month = factor(month(starttime), 
+                        levels = seq(1,12,1), 
+                        labels = c("Jan", "Feb", "Mar", 
+                                   "Apr", "Mai", "Jun", 
+                                   "Jul", "Aug", "Sep", 
+                                   "Oct", "Nov", "Dec"))
+         )
+
+
+
+describe(d.bike$month)
+
+
+
+###### DROP OUTLIERS IN COORDINATES ######
+# start_plot <- ggplot() +
+#   geom_point(
+#     aes(x=start_station_latitude, y=start_station_longitude), data=d.bike)
+# start_plot
+# 
+# end_plot <- ggplot() +
+#   geom_point(
+#     aes(x=end_station_latitude, y=end_station_longitude), data=d.bike)
+# end_plot
+
+d.bike <- filter(d.bike, 
+                  start_station_latitude > 40.6,
+                  end_station_latitude > 40.6
+                  )
+# 
+# start_plot2 <- ggplot() +
+#   geom_point(
+#     aes(x=start_station_latitude, y=start_station_longitude), data=d.bike)
+# # start_plot2
+# 
+# end_plot2 <- ggplot() +
+#   geom_point(
+#     aes(x=end_station_latitude, y=end_station_longitude), data=d.bike)
+# # end_plot2
+
+###### FILTER SUBSCRIBERS ######
+d.bike <- filter(d.bike,
+                 usertype == "Subscriber"
+                 )
+
+
+
+
+
+
+d.weather <- read_csv("data/weather_nyc_2016.csv")
+date <- dmy(d.weather$date)
+d.weather$date <- NULL
+d.weather$date <- date# join 2 dataset, bike sharing & weather
+d.bike_weather <- merge(x=d.bike.raw, y=d.weather, by.x=c("start_date"), by.y=c("date"), all=FALSE)
+saveRDS(d.bike_weather, file = "d.bike_weather.rds")
+
+
+saveRDS(d.bike, file = "../data/d.bike.prepared.rds")
---
+++ bike-share-analysis/Rscript/d.weather_preprocessing.R
@@ -0,0 +1,37 @@
+# Install and load packages
+
+# library(checkpoint)
+# checkpoint(snapshotDate = "2020-01-01")
+
+###### LOAD PACKAGES ######
+# create function eventually install and load all packages <- 
+if(!require("tidyverse")) install.packages("tidyverse")
+library("tidyverse")
+library("lubridate")
+library("Hmisc")
+
+###### LOAD DATA ######
+# setwd("C:/Users/Andy Gubser/OneDrive - Hochschule Luzern/01 Studium/03 MSc Data Science/Master HS19/Wahlpflichtmodule/W.MSCIDS_RB01.H1901/Assignment/R_bootcamp/bike-share-analysis/Rscript")
+d.weather.raw <- read_csv("../data/weather_nyc_2016.csv")
+colnames(d.weather.raw)
+describe(d.weather.raw)
+
+###### CONVERT DATA ######
+d.weather <- d.weather.raw %>% select_all(snakecase::to_snake_case)
+colnames(d.weather)
+describe(d.weather)
+
+###### CREATE DATE ######
+d.weather$date <- dmy(d.weather$date)
+describe(d.weather$date)
+
+###### MERGE WEATHER AND BIKE DATA ######
+d.bike_weather <- base::merge(x=d.bike, y=d.weather, 
+                        by.x=c("startdate"), 
+                        by.y=c("date")
+                        )
+
+colnames(d.bike_weather)
+
+saveRDS(d.bike_weather, file = "d.bike_weather.rds")
+
